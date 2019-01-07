4b9ffd849bc2ea1f6db9d1e89f5dbf641bca20d9
Josef Staroba <jstaroba@centene.com>
2019-01-07 11:26:44 -0600

    Add another test to get more stuck

--- src/wrapper.spec.ts
+++ src/wrapper.spec.ts
@@ -1,9 +1,10 @@
 describe('Wrapper', () => {
   it('should wrap', () => {
     expect("word\nword").toEqual(wrap("word word", 4));
+    expect("a dog").toEqual(wrap("a dog", 5));
   });
 
   const wrap = (s: string, width: number): string => {
-    return s.replace(" ", "\n");
+    return (s.length > width) ? s.replace(" ", "\n") : s;
   }
 });
