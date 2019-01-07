6c585c2fdd0434a3f1a3b129b4707b4fbfe7e55d
Josef Staroba <jstaroba@centene.com>
2019-01-07 11:50:55 -0600

    Start with a degenerate test

--- src/wrapper.spec.ts
+++ src/wrapper.spec.ts
@@ -1,8 +1,9 @@
 describe('Wrapper', () => {
   it('should wrap', () => {
+    expect('').toEqual(wrap(null, 1));
   });
 
   const wrap = (s: string, width: number): string => {
-    return null;
+    return '';
   }
 });
