4e135c01694de41525bb6588c44bdbb9fce50c03
Josef Staroba <jstaroba@centene.com>
2019-01-07 11:53:35 -0600

    another degenerate test case

--- src/wrapper.spec.ts
+++ src/wrapper.spec.ts
@@ -1,6 +1,7 @@
 describe('Wrapper', () => {
   it('should wrap', () => {
     expect('').toEqual(wrap(null, 1));
+    expect('').toEqual(wrap('', 1));
   });
 
   const wrap = (s: string, width: number): string => {
