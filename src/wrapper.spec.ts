8c379c660a5e3a4c639490454e8e2c42d7f16c29
Josef Staroba <jstaroba@centene.com>
2019-01-07 11:49:14 -0600

    Reset after getting stuck

--- src/wrapper.spec.ts
+++ src/wrapper.spec.ts
@@ -1,10 +1,8 @@
 describe('Wrapper', () => {
   it('should wrap', () => {
-    expect("word\nword").toEqual(wrap("word word", 4));
-    expect("a dog").toEqual(wrap("a dog", 5));
   });
 
   const wrap = (s: string, width: number): string => {
-    return (s.length > width) ? s.replace(" ", "\n") : s;
+    return null;
   }
 });
