e4749159ee319398e955896061ff1d9e2f6a79ca
Josef Staroba <jstaroba@centene.com>
2019-01-07 11:58:40 -0600

    add third degenerate test

--- src/wrapper.spec.ts
+++ src/wrapper.spec.ts
@@ -2,9 +2,12 @@ describe('Wrapper', () => {
   it('should wrap', () => {
     expect('').toEqual(wrap(null, 1));
     expect('').toEqual(wrap('', 1));
+    expect('x').toEqual(wrap('x', 1));
   });
 
   const wrap = (s: string, width: number): string => {
-    return '';
+    if (s === null)
+      return '';
+    return s;
   }
 });
